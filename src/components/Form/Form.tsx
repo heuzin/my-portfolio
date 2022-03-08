da1e0afb4c682e6e6d62ce4213ace78a3650c3c8
unknown <msilva@synergyonline.com>
2022-03-08 14:19:54 -0500

    recaptcha key

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -77,7 +77,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6LcAu-IdAAAAAJOTI5E_eRltZNQCvukIl2-f1glQ"
+          sitekey="6Lda-cMeAAAAAGHOSidK26gMdrn9MLImR24EvAWk"
           onChange={(e) => {
             setIsHuman(true);
           }}
